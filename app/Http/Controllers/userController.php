c108e4cac6b4adfc89b542d8d5a04da22904c79f
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-07-20 12:07:38 +0800

    ✨ feat: implement user registration controller

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class userController extends Controller
+{
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'name' => ['required', 'min:3', 'max:20'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'max:15']
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        User::create($incomingFields);
+
+        return redirect('/');
+    }
+}
