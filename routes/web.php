d8c93fe9d12a6ca24af59cafca2c7f443e02b1a0
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-07-21 10:09:16 +0800

    ✨ feat: add login route to userController for user authentication

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::get('/', function () {
 });
 
 Route::post('/register', [userController::class, 'register']);
+Route::post('/login', [userController::class, 'login']);
 Route::post('/logout', [userController::class, 'logout']);
