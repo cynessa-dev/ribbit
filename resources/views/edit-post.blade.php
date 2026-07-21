507311c82d10b64bb31aff257df2d5ddc8202e9a
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-07-21 11:55:10 +0800

    ✨ feat: create edit post view with form for updating post title and content

---
+++ resources/views/edit-post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ribbit</title>
+</head>
+<body>
+    <h1>Edit Post</h1>
+    <form action="/edit-post/{{ $post->id }}" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="title" value="{{ $post->title }}" />
+        <textarea name="body" placeholder="Content goes here...">{{ $post->body }}</textarea>
+        <button>Create Post</button>
+    </form>
+</body>
+</html>
